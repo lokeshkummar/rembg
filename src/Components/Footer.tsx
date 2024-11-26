d09f03a8930c7d3b6fad03c0b0f825b300a64642
Lokesh Kumar <lokeshkumar.080806@gmail.com>
2024-11-26 09:27:35 +0530

    change in footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -130,7 +130,7 @@ const Footer = () => {
         </ul>
       </div>
       <div className="w-full py-3 bg-[#000000] border-t border-slate-700 text-center">
-        <span className="text-xs text-[#02e797] opacity-50">Copyright &#169;rembg.in | All Rights Reserved. </span>
+        <span className="text-xs text-[#02e797] opacity-50">Designed & Developed by <Link to='https://portfolio-website-ten-pi.vercel.app/' className="hover:underline">Lokesh Kumar</Link> With ❤️</span>
       </div>
     </footer>
   );
